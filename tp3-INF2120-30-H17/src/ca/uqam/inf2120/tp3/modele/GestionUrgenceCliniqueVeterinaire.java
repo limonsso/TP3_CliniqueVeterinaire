d0d01d55c7e8a8b9f05eb6dec54554b52e358b90
limonsso <ezechiel.gnepa09@gmail.com>
2017-04-09 22:24:50 -0400

    GestionUrgenceCliniqueVeterinaire

--- tp3-INF2120-30-H17/src/ca/uqam/inf2120/tp3/modele/GestionUrgenceCliniqueVeterinaire.java
+++ tp3-INF2120-30-H17/src/ca/uqam/inf2120/tp3/modele/GestionUrgenceCliniqueVeterinaire.java
@@ -1,6 +1,5 @@
 package ca.uqam.inf2120.tp3.modele;
 
-
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
@@ -14,116 +13,128 @@ import ca.uqam.inf2120.tp3.adt.impl.FileAttenteImpl;
  *
  */
 public class GestionUrgenceCliniqueVeterinaire {
-	
-	//Declaration d'attributs
+
+	// Declaration d'attributs
 	private FileAttenteTda<Patient> listePatients = null;
-	
 
 	public GestionUrgenceCliniqueVeterinaire() {
 		listePatients = new FileAttenteImpl<Patient>();
 	}
+
 	/**
 	 * 
-	 * @param unPatient le patient a ajouté
+	 * @param unPatient
+	 *            le patient a ajouté
 	 */
-	public void placerPatient(Patient unPatient){
+	public void placerPatient(Patient unPatient) {
 		listePatients.placer(unPatient);
 	}
+
 	/**
 	 * Modifie le patient passé en paramètre
+	 * 
 	 * @param unPatient
 	 */
-	public void modifierPatient(Patient unPatient){
+	public void modifierPatient(Patient unPatient) {
 		Patient patient = rechercherParIndetiant(unPatient.getIdentifiant());
-		if(patient != null){
+		if (patient != null) {
 			listePatients.enlever(patient);
 			patient.setAge(unPatient.getAge());
 			patient.modifierPriorite(unPatient.getPriorite());
 			patient.setRaisonUrgence(unPatient.getRaisonUrgence());
 			listePatients.placer(patient);
 		}
-		
+
 	}
+
 	/**
 	 * Recherche les patients en fonction du type de recherche.
+	 * 
 	 * @param Priorite
-	 * @param typeRecherche 
-	 * @return 	- Si typeRecherche = 1 retourne la list des patient dont la priorité est superieur a celle passé en paramètre
-	 * 			- Si typeRecherche = 0 retourne la list des patient dont la priorité est égale a celle passé en paramètre
-	 * 			- Si typeRecherche = -1 retourne la list des patient dont la priorité est inferieur a celle passé en paramètre
-	 * 			- null si aucun patient n'a été trouvé
+	 * @param typeRecherche
+	 * @return - Si typeRecherche = 1 retourne la list des patient dont la
+	 *         priorité est superieur a celle passé en paramètre - Si
+	 *         typeRecherche = 0 retourne la list des patient dont la priorité
+	 *         est égale a celle passé en paramètre - Si typeRecherche = -1
+	 *         retourne la list des patient dont la priorité est inferieur a
+	 *         celle passé en paramètre - null si aucun patient n'a été trouvé
 	 */
-	public List<Patient> rechercheParPriorite(int Priorite,int typeRecherche){
-		List<Patient> lPatients= new ArrayList<Patient>();
-		Iterator<Patient> itPatient =  listePatients.iterateur();				
+	public List<Patient> rechercheParPriorite(int Priorite, int typeRecherche) {
+		List<Patient> lPatients = new ArrayList<Patient>();
+		Iterator<Patient> itPatient = listePatients.iterateur();
 		Patient patient = null;
-		switch(typeRecherche)
-		{
-			case 1:
-				while(itPatient.hasNext()){
-					patient = itPatient.next();
-					if(patient.getPriorite() > Priorite){
-						lPatients.add(patient);
-					}
+		switch (typeRecherche) {
+		case 1:
+			while (itPatient.hasNext()) {
+				patient = itPatient.next();
+				if (patient.getPriorite() > Priorite) {
+					lPatients.add(patient);
 				}
-				break;
-			case -1:
-				while(itPatient.hasNext()){
-					patient = itPatient.next();
-					if(patient.getPriorite() < Priorite){
-						lPatients.add(patient);
-					}
+			}
+			break;
+		case -1:
+			while (itPatient.hasNext()) {
+				patient = itPatient.next();
+				if (patient.getPriorite() < Priorite) {
+					lPatients.add(patient);
 				}
-				break;
-			case 0:
-				while(itPatient.hasNext()){
-					patient = itPatient.next();
-					if(patient.getPriorite() == Priorite){
-						lPatients.add(patient);
-					}
+			}
+			break;
+		case 0:
+			while (itPatient.hasNext()) {
+				patient = itPatient.next();
+				if (patient.getPriorite() == Priorite) {
+					lPatients.add(patient);
 				}
-				break;
+			}
+			break;
 		}
-		
-		return lPatients.size()==0 ? null : lPatients;
+
+		return lPatients.size() == 0 ? null : lPatients;
 	}
+
 	/**
 	 * 
 	 * @param identifiant
-	 * @return patient : le patient ayant l'indentifiant recherché ou null si aucun patient trouvé
+	 * @return patient : le patient ayant l'indentifiant recherché ou null si
+	 *         aucun patient trouvé
 	 */
-	public Patient rechercherParIndetiant(String identifiant){
+	public Patient rechercherParIndetiant(String identifiant) {
 		boolean bTrouve = false;
-		Iterator<Patient> itPatient =  listePatients.iterateur();
+		Iterator<Patient> itPatient = listePatients.iterateur();
 		Patient patient = null;
-		while(itPatient.hasNext() && !bTrouve){
+		while (itPatient.hasNext() && !bTrouve) {
 			patient = itPatient.next();
-			if(patient.getIdentifiant()==identifiant){
+			if (patient.getIdentifiant() == identifiant) {
 				bTrouve = true;
 			}
 		}
 		return patient;
 	}
+
 	/**
 	 * Recherche Tous les patients enregistrés
+	 * 
 	 * @return retourne tous les patients ou null si aucun patient n'existe
 	 */
-	public List<Patient> rechercheTousLesPatients(){
-		List<Patient> lPatients= new ArrayList<Patient>();
-		Iterator<Patient> itPatient =  listePatients.iterateur();				
+	public List<Patient> rechercheTousLesPatients() {
+		List<Patient> lPatients = new ArrayList<Patient>();
+		Iterator<Patient> itPatient = listePatients.iterateur();
 		Patient patient = null;
-		while(itPatient.hasNext()){
+		while (itPatient.hasNext()) {
 			patient = itPatient.next();
 			lPatients.add(patient);
 		}
-		return lPatients.size()==0 ? null : lPatients;
+		return lPatients.size() == 0 ? null : lPatients;
 	}
+
 	/**
 	 * Enlève
+	 * 
 	 * @param patient
 	 * @return
 	 */
-	public boolean enleverPatient(Patient patient){
+	public boolean enleverPatient(Patient patient) {
 		return listePatients.enlever(patient);
 	}
 }
