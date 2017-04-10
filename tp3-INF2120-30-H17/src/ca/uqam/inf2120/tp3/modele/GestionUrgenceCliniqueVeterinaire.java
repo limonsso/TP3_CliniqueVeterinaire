223f1d621a2c359f33e2252cd392921abecee46a
limonsso <ezechiel.gnepa09@gmail.com>
2017-04-09 22:20:33 -0400

    GestionUrgenceCliniqueVeterinaire -modifierPatient -rechercheParPriorite -rechercheTousLesPatients

--- tp3-INF2120-30-H17/src/ca/uqam/inf2120/tp3/modele/GestionUrgenceCliniqueVeterinaire.java
+++ tp3-INF2120-30-H17/src/ca/uqam/inf2120/tp3/modele/GestionUrgenceCliniqueVeterinaire.java
@@ -1,6 +1,7 @@
 package ca.uqam.inf2120.tp3.modele;
 
 
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
@@ -17,7 +18,7 @@ public class GestionUrgenceCliniqueVeterinaire {
 	//Declaration d'attributs
 	private FileAttenteTda<Patient> listePatients = null;
 	
-	/*********get*******/
+
 	public GestionUrgenceCliniqueVeterinaire() {
 		listePatients = new FileAttenteImpl<Patient>();
 	}
@@ -28,13 +29,63 @@ public class GestionUrgenceCliniqueVeterinaire {
 	public void placerPatient(Patient unPatient){
 		listePatients.placer(unPatient);
 	}
-	
+	/**
+	 * Modifie le patient passé en paramètre
+	 * @param unPatient
+	 */
 	public void modifierPatient(Patient unPatient){
+		Patient patient = rechercherParIndetiant(unPatient.getIdentifiant());
+		if(patient != null){
+			listePatients.enlever(patient);
+			patient.setAge(unPatient.getAge());
+			patient.modifierPriorite(unPatient.getPriorite());
+			patient.setRaisonUrgence(unPatient.getRaisonUrgence());
+			listePatients.placer(patient);
+		}
 		
 	}
+	/**
+	 * Recherche les patients en fonction du type de recherche.
+	 * @param Priorite
+	 * @param typeRecherche 
+	 * @return 	- Si typeRecherche = 1 retourne la list des patient dont la priorité est superieur a celle passé en paramètre
+	 * 			- Si typeRecherche = 0 retourne la list des patient dont la priorité est égale a celle passé en paramètre
+	 * 			- Si typeRecherche = -1 retourne la list des patient dont la priorité est inferieur a celle passé en paramètre
+	 * 			- null si aucun patient n'a été trouvé
+	 */
 	public List<Patient> rechercheParPriorite(int Priorite,int typeRecherche){
+		List<Patient> lPatients= new ArrayList<Patient>();
+		Iterator<Patient> itPatient =  listePatients.iterateur();				
+		Patient patient = null;
+		switch(typeRecherche)
+		{
+			case 1:
+				while(itPatient.hasNext()){
+					patient = itPatient.next();
+					if(patient.getPriorite() > Priorite){
+						lPatients.add(patient);
+					}
+				}
+				break;
+			case -1:
+				while(itPatient.hasNext()){
+					patient = itPatient.next();
+					if(patient.getPriorite() < Priorite){
+						lPatients.add(patient);
+					}
+				}
+				break;
+			case 0:
+				while(itPatient.hasNext()){
+					patient = itPatient.next();
+					if(patient.getPriorite() == Priorite){
+						lPatients.add(patient);
+					}
+				}
+				break;
+		}
 		
-		return null;
+		return lPatients.size()==0 ? null : lPatients;
 	}
 	/**
 	 * 
@@ -53,10 +104,25 @@ public class GestionUrgenceCliniqueVeterinaire {
 		}
 		return patient;
 	}
+	/**
+	 * Recherche Tous les patients enregistrés
+	 * @return retourne tous les patients ou null si aucun patient n'existe
+	 */
 	public List<Patient> rechercheTousLesPatients(){
-		
-		return null;
+		List<Patient> lPatients= new ArrayList<Patient>();
+		Iterator<Patient> itPatient =  listePatients.iterateur();				
+		Patient patient = null;
+		while(itPatient.hasNext()){
+			patient = itPatient.next();
+			lPatients.add(patient);
+		}
+		return lPatients.size()==0 ? null : lPatients;
 	}
+	/**
+	 * Enlève
+	 * @param patient
+	 * @return
+	 */
 	public boolean enleverPatient(Patient patient){
 		return listePatients.enlever(patient);
 	}
