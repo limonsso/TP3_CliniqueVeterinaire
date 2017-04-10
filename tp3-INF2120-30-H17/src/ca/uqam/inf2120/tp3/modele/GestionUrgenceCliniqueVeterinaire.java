3276c8b8864f147e1be3b3c1d5b8547d21fe72c8
limonsso <ezechiel.gnepa09@gmail.com>
2017-04-09 21:05:30 -0400

    GestionUrgenceCliniqueVeterinaire

--- tp3-INF2120-30-H17/src/ca/uqam/inf2120/tp3/modele/GestionUrgenceCliniqueVeterinaire.java
+++ tp3-INF2120-30-H17/src/ca/uqam/inf2120/tp3/modele/GestionUrgenceCliniqueVeterinaire.java
@@ -1,22 +1,32 @@
 package ca.uqam.inf2120.tp3.modele;
 
 
+import java.util.Iterator;
 import java.util.List;
 
 import ca.uqam.inf2120.tp1.adt.FileAttenteTda;
 import ca.uqam.inf2120.tp3.adt.impl.FileAttenteImpl;
 
+/**
+ * 
+ * @author Ezechiel Gneap
+ *
+ */
 public class GestionUrgenceCliniqueVeterinaire {
 	
 	//Declaration d'attributs
-	private FileAttenteTda listePatients;
+	private FileAttenteTda<Patient> listePatients = null;
 	
-	
+	/*********get*******/
 	public GestionUrgenceCliniqueVeterinaire() {
-		listePatients = new FileAttenteImpl();
+		listePatients = new FileAttenteImpl<Patient>();
 	}
+	/**
+	 * 
+	 * @param unPatient le patient a ajouté
+	 */
 	public void placerPatient(Patient unPatient){
-		
+		listePatients.placer(unPatient);
 	}
 	
 	public void modifierPatient(Patient unPatient){
@@ -26,14 +36,28 @@ public class GestionUrgenceCliniqueVeterinaire {
 		
 		return null;
 	}
+	/**
+	 * 
+	 * @param identifiant
+	 * @return patient : le patient ayant l'indentifiant recherché ou null si aucun patient trouvé
+	 */
 	public Patient rechercherParIndetiant(String identifiant){
-		return null;
+		boolean bTrouve = false;
+		Iterator<Patient> itPatient =  listePatients.iterateur();
+		Patient patient = null;
+		while(itPatient.hasNext() && !bTrouve){
+			patient = itPatient.next();
+			if(patient.getIdentifiant()==identifiant){
+				bTrouve = true;
+			}
+		}
+		return patient;
 	}
 	public List<Patient> rechercheTousLesPatients(){
 		
 		return null;
 	}
 	public boolean enleverPatient(Patient patient){
-		return false;
+		return listePatients.enlever(patient);
 	}
 }
